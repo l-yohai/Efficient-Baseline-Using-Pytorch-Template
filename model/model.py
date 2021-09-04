8953e4e6087637190a3c535126586ba1a4679a9b
l-yohai <yohan9612@naver.com>
2021-09-04 21:42:10 +0000

    feat: custom model

--- model/model.py
+++ model/model.py
@@ -1,22 +1,24 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
+import timm
 
 
-class MnistModel(BaseModel):
-    def __init__(self, num_classes=10):
+class Model(BaseModel):
+    def __init__(self, num_classes=10, pretrained_model='efficientnet_b0'):
+        self.check_args(pretrained_model)
+
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_classes)
+        self.pretrained_model = timm.create_model(
+            pretrained_model, pretrained=True)
+        # TODO: Your pretrained model's out features
+        self.fc = nn.Linear(1000, num_classes)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        output = self.pretrained_model(x)
+        output = self.fc(output)
+        return output
+
+    def check_args(self, pretrained_model):
+        if not timm.is_model(pretrained_model):
+            assert "Model does not create from timm.\nPlease check the model name."
