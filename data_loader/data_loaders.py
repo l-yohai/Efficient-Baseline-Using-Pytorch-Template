fea5962b5e47cc2572d75f8469571739b1be3ea3
l-yohai <yohan9612@naver.com>
2021-09-04 21:41:31 +0000

    feat: custom dataloader

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,16 +1,51 @@
 from torchvision import datasets, transforms
 from base import BaseDataLoader
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from custom_dataset import *
 
 
-class MnistDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
+# TODO: Edit augmentations you want
+transform_list = [
+    A.Compose([
+        A.Rotate(limit=20, p=1),
+        A.CenterCrop(height=224, width=224, p=1),
+    ]),
+    A.Compose([
+        A.Rotate(limit=10, p=1),
+        A.GridDistortion(always_apply=False, p=1.0, num_steps=5, distort_limit=(
+            -0.3, 0.3), interpolation=0, border_mode=0, value=(0, 0, 0), mask_value=None)
+    ])
+]
+
+
+class CustomDataLoader(BaseDataLoader):
+    def __init__(self, data_dir, csv_path, batch_size, shuffle=True, validation_split=0.0, num_workers=2, training=True):
+        # TODO: Edit augmentations you want
+        self.transform = A.Compose([
+            A.OneOf(transform_list, p=0.5),
+            A.Resize(224, 224),
+            A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.2, 0.2, 0.2)),
+            ToTensorV2()
         ])
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        self.csv_path = csv_path
+
+        self.dataset = CustomDatasetFromImages(
+            self.data_dir, self.csv_path, self.transform, train=training)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class CustomValidDataLoader(BaseDataLoader):
+    def __init__(self, data_dir, csv_path, batch_size, shuffle=True, validation_split=0.0, num_workers=2, training=False):
+        # TODO: Edit augmentations you want
+        self.transform = A.Compose([
+            A.Resize(224, 224),
+            A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.2, 0.2, 0.2)),
+            ToTensorV2()
+        ])
+        self.data_dir = data_dir
+        self.csv_path = csv_path
+        self.dataset = CustomValidDatasetFromImages(
+            self.data_dir, self.csv_path, self.transform)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
